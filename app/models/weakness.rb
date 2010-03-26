46dfca1eef2ba8507708bcc98d3179eed8a68d59
Michael Glass <me@mike.is>
2010-03-26 17:37:05 -0400

    wrote code (in Weakness.rb) to update count & average score.  dumped sql to static_data/holey moley data.sql

--- app/models/weakness.rb
+++ app/models/weakness.rb
@@ -12,4 +12,24 @@ class Weakness < ActiveRecord::Base
   has_many :vulnerabilities
   
   WEAKNESS_IDS = {'View' => 0, 'Category' => 1, 'Weakness' => 2, 'Compound_Element' => 3}
+  
+  def self.update_count_and_score
+    start_time = Time.now
+    print "recalculating average weakness scores and counts..."
+    Weakness.all.each do |weakness|
+      weakness.score = 0.0;
+      weakness.count = 0
+      weakness.vulnerabilities.each do |vulnerability|
+        weakness.count += 1;
+        weakness.score += vulnerability.score
+      end
+  
+      if weakness.count > 0
+        weakness.score /= weakness.count
+        weakness.save
+      end
+    end
+    puts "done, #{Time.now - start_time} seconds elapsed."
+  end
+  
 end
