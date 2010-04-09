71c4b5d83301ecef01f4526dd3a7434e0b5ae62f
Michael Glass <me@mike.is>
2010-04-09 13:43:52 -0400

    ok, now we populate suites and tests correctly

--- app/controllers/suites_controller.rb
+++ app/controllers/suites_controller.rb
@@ -42,21 +42,52 @@ class SuitesController < ApplicationController
   def create
     @suite = Suite.new(params[:suite])
     vulnerabilities = Vulnerability.find(:all, :conditions => {:published => @suite.start..@suite.end } )
-    weaknesses = {}
+    #weaknesses is a hash mapping weaknesses to the vulnerabilities they map to, ie Sequel Injection => [a, b, c...]
+    weaknesses = {} 
     averages = {}
     vulnerabilities.each do |vulnerability|
-      if weaknesses.has_key? vulnerability.weakness
-        weaknesses[vulnerability.weakness].push vulnerability
+      if weaknesses.has_key? vulnerability.weakness.id
+        weaknesses[vulnerability.weakness.id].push vulnerability
         #updated averages
-        averages[vulnerability.weakness] += vulnerability.score
+        averages[vulnerability.weakness.id] += vulnerability.score
       else
-        weaknesses[vulnerability.weakness] = [vulnerability]
-        averages[vulnerability.weakness] = vulnerability.score
+        weaknesses[vulnerability.weakness.id] = [vulnerability]
+        averages[vulnerability.weakness.id] = vulnerability.score
         #set initial average...
       end
     end
     averages.each { |k, v| averages[k] = v/weaknesses[k].size }
+    
+    #find ranks
+    #in array, insert all vulnerability/count pairs, sort by pairs.
+    ranks = []
+    weaknesses.each do |weakness, vulnerabilities|
+      count = vulnerabilities.length
+      ranks.push [weakness, count]
+    end
+    
+    ranks.sort! {|a, b| a[1] <=> b[1]}
+
+
+    rank_multiplier = ranks.length/10.0
+    
+    ranks.each_with_index do |rank, index|
+      weaknesses[rank[0]] = (index + 1) * rank_multiplier
+    end
+    
+    ranks.clear
+    
+    weaknesses.each do |weakness, rank|
+      score = rank * @suite.weight + averages[weakness] * (1.0 - @suite.weight)
+      ranks.push [weakness, score]
+    end
+    
     #now sort, choose top weaknesses...
+    ranks.sort! {|a,b| b[1] <=> a[1]} #sorting highest to lowest.
+    ranks[0..@suite.top].each do |top_rated|
+      @suite.tests.build( :weakness_id => top_rated[0] )
+    end
+        
     respond_to do |format|
       if @suite.save
         flash[:notice] = 'Suite was successfully created.'
